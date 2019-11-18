d1c32ac0041475580e68db62656439d642f8d6a6
Bilel Ouri <ouribilel@gmail.com>
2019-11-18 16:10:40 +0100

    stupid

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,7 +6,7 @@ class QuestionsController < ApplicationController
     @answer = params[:question]
     if @answer == "I am going to work"
       @response = "Great!"
-    elsif answer.include? "?"
+    elsif @answer.include? "?"
       @response = "Silly question, get dressed and go to work!"
     else
       @response = "I don't care, get dressed and go to work!"
